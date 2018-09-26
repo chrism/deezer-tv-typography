b8c2231838370a903f200efbd0ce217e786f56c9
Chris Masters <chrismasters@gmail.com>
2018-09-26 19:08:50 +0200

    Working stacks

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('font-family-audit', { path: '/' });
+  this.route('testing-stacks', { path: '/stacks' });
 });
 
 export default Router;
