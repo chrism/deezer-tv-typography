d089dd97f2a6f617ac98397f5f5451f1b5d5d28d
Chris Masters <chrismasters@gmail.com>
2018-09-28 15:48:57 +0200

    First pass at trying to prerender html

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    includeHighlightStyle: false
+    includeHighlightStyle: false,
+    prember: {
+      urls: [
+        '/',
+        '/stacks'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
