da778fcc93022a600783bfa9a1fa8dfb59013158
Chris Masters <chrismasters@gmail.com>
2018-09-28 17:43:51 +0200

    Working without javascript...

--- app/components/font-sample.js
+++ app/components/font-sample.js
@@ -3,16 +3,13 @@ import { computed } from '@ember/object';
 import { htmlSafe } from '@ember/string';
 
 export default Component.extend({
-  showCode: false,
   classNames: ['mb-8'],
 
   computedStyle: computed('css', function() {
     return htmlSafe(this.css);
   }),
 
-  actions: {
-    toggleCode() {
-      this.toggleProperty('showCode');
-    }
-  }
+  uniqId: computed('elementId', function() {
+    return `id_${this.elementId}`;
+  })
 });
